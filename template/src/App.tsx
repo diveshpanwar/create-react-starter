00b9dbe6b4402fbf6f17d76d247d08141bce469a
Divesh Panwar <divesh.panwar@gmail.com>
2024-10-23 15:21:06 +0530

    Updated generated Flags

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +28,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <img
+        alt="Generated Using create-app-using-react"
+        src="https://img.shields.io/badge/Generated-create--app--using--react-Green?style=for-the-badge"
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
