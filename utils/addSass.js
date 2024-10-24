decb5bbca84a2c852849ae7e264e91a367758fda
Divesh Panwar <divesh.panwar@gmail.com>
2024-10-24 12:43:17 +0530

    Updated Files wrt Prettier

--- utils/addSass.js
+++ utils/addSass.js
@@ -1,106 +1,106 @@
-import chalk from "chalk";
-import fs from "fs-extra";
-import path from "path";
-import { packageVersions } from "../config.js";
+import chalk from 'chalk';
+import fs from 'fs-extra';
+import path from 'path';
+import { packageVersions } from '../config.js';
 
 // Function to update package.json with Sass
 export async function addSassToPackageJson(targetDir) {
-  const packageJsonPath = path.join(targetDir, "package.json");
+    const packageJsonPath = path.join(targetDir, 'package.json');
 
-  try {
-    // Read the existing package.json
-    console.log(chalk.cyan(`Updating ${chalk.yellow("package.json")}`));
-    const packageJson = await fs.readJson(packageJsonPath);
+    try {
+        // Read the existing package.json
+        console.log(chalk.cyan(`Updating ${chalk.yellow('package.json')}`));
+        const packageJson = await fs.readJson(packageJsonPath);
 
-    // Add Sass as a dependency with the latest compatible version
-    packageJson.dependencies = packageJson.dependencies || {};
-    packageJson.dependencies["sass"] = packageVersions["sass"]; // Or use any desired version or latest
+        // Add Sass as a dependency with the latest compatible version
+        packageJson.dependencies = packageJson.dependencies || {};
+        packageJson.dependencies['sass'] = packageVersions['sass']; // Or use any desired version or latest
 
-    // Write the updated package.json back
-    await fs.writeJson(packageJsonPath, packageJson, { spaces: 2 });
-    console.log(chalk.cyan("Added Sass to project"));
-  } catch (err) {
-    console.error(chalk.red(`Error updating package.json: ${err}`));
-  }
+        // Write the updated package.json back
+        await fs.writeJson(packageJsonPath, packageJson, { spaces: 2 });
+        console.log(chalk.cyan('Added Sass to project'));
+    } catch (err) {
+        console.error(chalk.red(`Error updating package.json: ${err}`));
+    }
 }
 
 // Function to rename .css files to .scss
 export async function renameCssToScss(targetDir) {
-  const indexCssPath = path.join(targetDir, "src/index.css");
-  const appCssPath = path.join(targetDir, "src/App.css");
+    const indexCssPath = path.join(targetDir, 'src/index.css');
+    const appCssPath = path.join(targetDir, 'src/App.css');
 
-  const indexScssPath = path.join(targetDir, "src/index.scss");
-  const appScssPath = path.join(targetDir, "src/App.scss");
+    const indexScssPath = path.join(targetDir, 'src/index.scss');
+    const appScssPath = path.join(targetDir, 'src/App.scss');
 
-  try {
-    // Rename index.css to index.scss
-    if (await fs.pathExists(indexCssPath)) {
-      await fs.rename(indexCssPath, indexScssPath);
-      console.log(
-        chalk.cyan(
-          `Renamed ${chalk.yellow("index.css")} to ${chalk.yellow(
-            "index.scss"
-          )}`
-        )
-      );
+    try {
+        // Rename index.css to index.scss
+        if (await fs.pathExists(indexCssPath)) {
+            await fs.rename(indexCssPath, indexScssPath);
+            console.log(
+                chalk.cyan(
+                    `Renamed ${chalk.yellow('index.css')} to ${chalk.yellow(
+                        'index.scss'
+                    )}`
+                )
+            );
+        }
+
+        // Rename App.css to App.scss
+        if (await fs.pathExists(appCssPath)) {
+            await fs.rename(appCssPath, appScssPath);
+            console.log(
+                chalk.cyan(
+                    `Renamed ${chalk.yellow('App.css')} to ${chalk.yellow('App.scss')}`
+                )
+            );
+        }
+    } catch (err) {
+        console.error(chalk.red(`Error renaming CSS files to SCSS: ${err}`));
     }
-
-    // Rename App.css to App.scss
-    if (await fs.pathExists(appCssPath)) {
-      await fs.rename(appCssPath, appScssPath);
-      console.log(
-        chalk.cyan(
-          `Renamed ${chalk.yellow("App.css")} to ${chalk.yellow("App.scss")}`
-        )
-      );
-    }
-  } catch (err) {
-    console.error(chalk.red(`Error renaming CSS files to SCSS: ${err}`));
-  }
 }
 
 export // Function to update imports in main.tsx and App.tsx
 async function updateImportsToScss(targetDir) {
-  const mainTsxPath = path.join(targetDir, "src/main.tsx");
-  const appTsxPath = path.join(targetDir, "src/App.tsx");
+    const mainTsxPath = path.join(targetDir, 'src/main.tsx');
+    const appTsxPath = path.join(targetDir, 'src/App.tsx');
 
-  try {
-    // Update main.tsx
-    if (await fs.pathExists(mainTsxPath)) {
-      let mainTsxContent = await fs.readFile(mainTsxPath, "utf8");
-      mainTsxContent = mainTsxContent.replace(
-        `import './index.css'`,
-        `import './index.scss'`
-      );
-      await fs.writeFile(mainTsxPath, mainTsxContent);
-      console.log(
-        chalk.cyan(
-          `Updated ${chalk.yellow("main.tsx")} to import ${chalk.yellow(
-            "index.scss"
-          )}`
-        )
-      );
+    try {
+        // Update main.tsx
+        if (await fs.pathExists(mainTsxPath)) {
+            let mainTsxContent = await fs.readFile(mainTsxPath, 'utf8');
+            mainTsxContent = mainTsxContent.replace(
+                `import './index.css'`,
+                `import './index.scss'`
+            );
+            await fs.writeFile(mainTsxPath, mainTsxContent);
+            console.log(
+                chalk.cyan(
+                    `Updated ${chalk.yellow('main.tsx')} to import ${chalk.yellow(
+                        'index.scss'
+                    )}`
+                )
+            );
+        }
+
+        // Update App.tsx
+        if (await fs.pathExists(appTsxPath)) {
+            let appTsxContent = await fs.readFile(appTsxPath, 'utf8');
+            appTsxContent = appTsxContent.replace(
+                `import './App.css'`,
+                `import './App.scss'`
+            );
+            await fs.writeFile(appTsxPath, appTsxContent);
+            console.log(
+                chalk.cyan(
+                    `Updated ${chalk.yellow('App.tsx')} to import ${chalk.yellow(
+                        'App.scss'
+                    )}`
+                )
+            );
+        }
+
+        console.log(chalk.magenta('Added SASS to the project.'));
+    } catch (err) {
+        console.error(chalk.red(`Error updating imports to .scss: ${err}`));
     }
-
-    // Update App.tsx
-    if (await fs.pathExists(appTsxPath)) {
-      let appTsxContent = await fs.readFile(appTsxPath, "utf8");
-      appTsxContent = appTsxContent.replace(
-        `import './App.css'`,
-        `import './App.scss'`
-      );
-      await fs.writeFile(appTsxPath, appTsxContent);
-      console.log(
-        chalk.cyan(
-          `Updated ${chalk.yellow("App.tsx")} to import ${chalk.yellow(
-            "App.scss"
-          )}`
-        )
-      );
-    }
-
-    console.log(chalk.magenta("Added SASS to the project."));
-  } catch (err) {
-    console.error(chalk.red(`Error updating imports to .scss: ${err}`));
-  }
 }
--- utils/common.js
+++ utils/common.js
@@ -1,27 +1,27 @@
-import chalk from "chalk";
-import fs from "fs-extra";
+import chalk from 'chalk';
+import fs from 'fs-extra';
 
 export const insertLine = () => {
-  console.log("");
-  const line = chalk.grey("_______________________________________________");
-  console.log(line);
-  console.log("");
+    console.log('');
+    const line = chalk.grey('_______________________________________________');
+    console.log(line);
+    console.log('');
 };
 
 export async function createFolder(folderPath) {
-  if (!fs.existsSync(folderPath)) {
-    fs.mkdirSync(folderPath, { recursive: true });
-  }
+    if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath, { recursive: true });
+    }
 }
 
 export function copyFile(source, destination) {
-  fs.copyFileSync(source, destination);
+    fs.copyFileSync(source, destination);
 }
 
 export const copyFolderStructure = (srcFolder, destFolder) => {
-  try {
-    fs.copySync(srcFolder, destFolder, { overwrite: true });
-  } catch (err) {
-    console.error(`Error copying folder structure: ${err}`);
-  }
+    try {
+        fs.copySync(srcFolder, destFolder, { overwrite: true });
+    } catch (err) {
+        console.error(`Error copying folder structure: ${err}`);
+    }
 };
